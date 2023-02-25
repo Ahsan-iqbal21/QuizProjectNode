bf08fc68a757d2171ad2957e1ccc1d0bd7f4234f
Ahsan Iqbal <ahsaniqbal112211@gmail.com>
2023-02-26 00:36:32 +0500

    database url added

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,8 @@ require('dotenv').config({path: '../.env'})
 const { Client } = require('pg');
 
 const client = new Client({
-    connectionString: process.env.DATABASE_URL,
+    // connectionString: process.env.DATABASE_URL,
+    connectionString: 'postgres://bjoaqvdezqbfhn:96106df7ca2a2962f3f7043f15f1526ca871f7b6f421eac5815c560caba8f681@ec2-3-218-171-44.compute-1.amazonaws.com:5432/d9te73q6ml6t9g',
     ssl: {
         rejectUnauthorized: false
     }
